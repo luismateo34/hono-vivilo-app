41e1b6afcd827748ef9ba4bfb84600223ee31df4
Luis Mateo Piñeiro <luismateo3461@gmail.com>
2026-05-19 00:49:16 -0300

    singletone de sequelize listo

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
-import {envObj } from "src/envObj";
+import { DotenvObj } from "src/utils/dotenv";
 import { Sequelize } from "sequelize-typescript";
 import pg from "pg";
+DotenvObj.getInstance()
 
 class SequelizeSingleton {
   static instance: SequelizeSingleton
-  private sequelize = new Sequelize(envObj.POSTGRES_URL, {
+  private sequelize = new Sequelize(process.env.POSTGRES_URL, {
         dialect: "postgres",
         repositoryMode:true,
         dialectModule: pg,
